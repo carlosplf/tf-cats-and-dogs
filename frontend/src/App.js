17c1a934156d048bda8277ec66dc8ad5667c8a6f
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2023-04-15 15:05:37 -0300

    Add frontend folder to project.

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header">
+        <p>Model</p>
+        <p>GitHub</p>
+        <p>About</p>
+      </div>
+      <h1 className="Title">Cats and Dogs!</h1>
+      <h2 className="SubTitle">Is it a Cat or a Dog?</h2>
+      <button id="SubmitButton">Send image</button>
+    </div>
+  );
+}
+
+export default App;
